b90dc9317b0f4f76507d57989007b2e2ee76d8c9
Zephyra <dibomaharani18@gmail.com>
2025-10-18 22:09:08 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,10 +17,10 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "keystore.jks")
-            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: ""
-            keyAlias = System.getenv("KEY_ALIAS") ?: ""
-            keyPassword = System.getenv("KEY_PASSWORD") ?: ""
+            storeFile = file(System.getenv("KEYSTORE_FILE"))
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
         }
         getByName("debug")
     }
@@ -39,8 +39,6 @@ android {
 repositories {
     google()
     mavenCentral()
-    mavenLocal()
-    maven("https://jitpack.io")
 }
 
 dependencies {
