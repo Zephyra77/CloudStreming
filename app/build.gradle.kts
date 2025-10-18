984ec333231b16996f70843d30bcb60758971a48
Zephyra <dibomaharani18@gmail.com>
2025-10-19 00:16:48 +0700

    Fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,10 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import org.jetbrains.dokka.gradle.engine.parameters.KotlinPlatform
-import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
-
 }
 
 val javaTarget = JvmTarget.fromTarget(libs.versions.jvmTarget.get())
@@ -17,21 +14,15 @@ val prereleaseStoreFile: File? = File(tmpFilePath).listFiles()?.first()
 fun getGitCommitHash(): String {
     return try {
         val headFile = file("${project.rootDir}/.git/HEAD")
-
-        // Read the commit hash from .git/HEAD
         if (headFile.exists()) {
             val headContent = headFile.readText().trim()
             if (headContent.startsWith("ref:")) {
-                val refPath = headContent.substring(5) // e.g., refs/heads/main
+                val refPath = headContent.substring(5)
                 val commitFile = file("${project.rootDir}/.git/$refPath")
                 if (commitFile.exists()) commitFile.readText().trim() else ""
-            } else headContent // If it's a detached HEAD (commit hash directly)
-        } else {
-            "" // If .git/HEAD doesn't exist
-        }.take(7) // Return the short commit hash
-    } catch (_: Throwable) {
-        "" // Just return an empty string if any exception occurs
-    }
+            } else headContent
+        } else ""
+    }.take(7)
 }
 
 android {
@@ -40,9 +31,7 @@ android {
         unitTests.isReturnDefaultValues = true
     }
 
-    viewBinding {
-        enable = true
-    }
+    viewBinding { enable = true }
 
     signingConfigs {
         if (prereleaseStoreFile != null) {
@@ -68,14 +57,9 @@ android {
         resValue("string", "commit_hash", getGitCommitHash())
         resValue("bool", "is_prerelease", "false")
 
-        // Reads local.properties
         val localProperties = gradleLocalProperties(rootDir, project.providers)
 
-        buildConfigField(
-            "long",
-            "BUILD_DATE",
-            "${System.currentTimeMillis()}"
-        )
+        buildConfigField("long", "BUILD_DATE", "${System.currentTimeMillis()}")
         buildConfigField(
             "String",
             "SIMKL_CLIENT_ID",
@@ -86,6 +70,7 @@ android {
             "SIMKL_CLIENT_SECRET",
             "\"" + (System.getenv("SIMKL_CLIENT_SECRET") ?: localProperties["simkl.secret"]) + "\""
         )
+
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -141,15 +126,12 @@ android {
         checkReleaseBuilds = false
     }
 
-    buildFeatures {
-        buildConfig = true
-    }
+    buildFeatures { buildConfig = true }
 
     namespace = "com.lagradost.cloudstream3"
 }
 
 dependencies {
-    // Testing
     testImplementation(libs.junit)
     testImplementation(libs.json)
     androidTestImplementation(libs.core)
@@ -157,90 +139,68 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    // Android Core & Lifecycle
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.bundles.navigationKtx)
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.lifecycle.viewmodel.ktx)
 
-    // Design & UI
     implementation(libs.preference.ktx)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.swiperefreshlayout)
 
-    // Coil Image Loading
     implementation(libs.coil)
     implementation(libs.coil.network.okhttp)
 
-    // Media 3 (ExoPlayer)
     implementation(libs.bundles.media3)
     implementation(libs.video)
 
-    // PlayBack
-    implementation(libs.colorpicker) // Subtitle Color Picker
-    implementation(libs.newpipeextractor) // For Trailers
-    implementation(libs.juniversalchardet) // Subtitle Decoding
+    implementation(libs.colorpicker)
+    implementation(libs.newpipeextractor)
+    implementation(libs.juniversalchardet)
 
-    // FFmpeg Decoding
     implementation(libs.bundles.nextlibMedia3)
 
-    // Crash Reports (AcraApplication.kt)
     implementation(libs.acra.core)
     implementation(libs.acra.toast)
 
-    // UI Stuff
-    implementation(libs.shimmer) // Shimmering Effect (Loading Skeleton)
-    implementation(libs.palette.ktx) // Palette for Images -> Colors
+    implementation(libs.shimmer)
+    implementation(libs.palette.ktx)
     implementation(libs.tvprovider)
-    implementation(libs.overlappingpanels) // Gestures
-    implementation(libs.biometric) // Fingerprint Authentication
-    implementation(libs.previewseekbar.media3) // SeekBar Preview
-    implementation(libs.qrcode.kotlin) // QR Code for PIN Auth on TV
+    implementation(libs.overlappingpanels)
+    implementation(libs.biometric)
+    implementation(libs.previewseekbar.media3)
+    implementation(libs.qrcode.kotlin)
 
-    // Extensions & Other Libs
-    implementation(libs.rhino) // Run JavaScript
+    implementation(libs.rhino)
     implementation(libs.quickjs)
-    implementation(libs.fuzzywuzzy) // Library/Ext Searching with Levenshtein Distance
-    implementation(libs.safefile) // To Prevent the URI File Fu*kery
-    coreLibraryDesugaring(libs.desugar.jdk.libs.nio) // NIO Flavor Needed for NewPipeExtractor
+    implementation(libs.fuzzywuzzy)
+    implementation(libs.safefile)
+    coreLibraryDesugaring(libs.desugar.jdk.libs.nio)
     implementation(libs.conscrypt.android) {
-        version {
-            strictly("2.5.2")
-        }
-        because("2.5.3 crashes everything for everyone.")
-    } // To Fix SSL Fu*kery on Android 9
+        version { strictly("2.5.2") }
+    }
     implementation(libs.jackson.module.kotlin) {
-        version {
-            strictly("2.13.1")
-        }
-        because("Don't Bump Jackson above 2.13.1, Crashes on Android TV's and FireSticks that have Min API Level 25 or Less.")
-    } // JSON Parser
+        version { strictly("2.13.1") }
+    }
 
-    // Torrent Support
     implementation(libs.torrentserver)
-
-    // Downloading & Networking
     implementation(libs.work.runtime)
     implementation(libs.work.runtime.ktx)
-    implementation(libs.nicehttp) // HTTP Lib
+    implementation(libs.nicehttp)
 
     implementation(project(":library") {
-        // There does not seem to be a good way of getting the android flavor.
         val isDebug = gradle.startParameter.taskRequests.any { task ->
-            task.args.any { arg ->
-                arg.contains("debug", true)
-            }
+            task.args.any { arg -> arg.contains("debug", true) }
         }
-
         this.extra.set("isDebug", isDebug)
     })
 }
 
 tasks.register<Jar>("androidSourcesJar") {
     archiveClassifier.set("sources")
-    from(android.sourceSets.getByName("main").java.srcDirs) // Full Sources
+    from(android.sourceSets.getByName("main").java.srcDirs)
 }
 
 tasks.register<Copy>("copyJar") {
@@ -250,13 +210,10 @@ tasks.register<Copy>("copyJar") {
     )
     into("build/app-classes")
     include("classes.jar", "library-jvm*.jar")
-    // Remove the version
     rename("library-jvm.*.jar", "library-jvm.jar")
 }
 
-// Merge the app classes and the library classes into classes.jar
 tasks.register<Jar>("makeJar") {
-    // Duplicates cause hard to catch errors, better to fail at compile time.
     duplicatesStrategy = DuplicatesStrategy.FAIL
     dependsOn(tasks.getByName("copyJar"))
     from(
@@ -275,24 +232,5 @@ tasks.withType<KotlinJvmCompile> {
             "-Xannotation-default-target=param-property",
             "-opt-in=com.lagradost.cloudstream3.Prerelease"
         )
-    }
-}
-
-dokka {
-    moduleName = "App"
-    dokkaSourceSets {
-        main {
-            analysisPlatform = KotlinPlatform.JVM
-            documentedVisibilities(
-                VisibilityModifier.Public,
-                VisibilityModifier.Protected
-            )
-
-            sourceLink {
-                localDirectory = file("..")
-                remoteUrl("https://github.com/recloudstream/cloudstream/tree/master")
-                remoteLineSuffix = "#L"
-            }
-        }
     }
 }
