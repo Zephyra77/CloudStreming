f09dfca2cc5973586abb614f53c8dcf82990dd61
Zephyra <dibomaharani18@gmail.com>
2025-10-19 00:41:52 +0700

    Update build.gradle.kts

--- docs/build.gradle.kts
+++ docs/build.gradle.kts
@@ -1,13 +1,19 @@
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.dokka")
+    kotlin("jvm") version "1.9.24"
+    id("org.jetbrains.dokka") version "1.9.20"
+}
+
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
 }
 
 dependencies {
-    dokka(project(":app:"))
-    dokka(project(":library:"))
+    dokka(project(":app"))
+    dokka(project(":library"))
 }
 
 dokka {
-    moduleName = "Cloudstream"
+    moduleName.set("Cloudstream")
 }
